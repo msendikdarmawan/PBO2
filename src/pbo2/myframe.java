b436b8c7f86ba337067abdcdde7760840ecd8844
ramadhan <d_el12@yahoo.com>
2017-11-03 10:34:14 +0800

    membuat METHOD Cetak Attribute

--- src/pbo2/myframe.java
+++ src/pbo2/myframe.java
@@ -1,6 +1,13 @@
 package pbo2;
 public class myframe {
-    string pesan;
+    String pesan;
     int bilBulat;
     double bilDesimal;
+    
+    void cetakAttribute() {
+        System.out.println("isi dari pesan :"+pesan);
+        System.out.println("isi dari bilBulat :"+bilBulat);
+        System.out.println("isi dari bilDesimal :"+bilDesimal);
+        
+    }
 }
