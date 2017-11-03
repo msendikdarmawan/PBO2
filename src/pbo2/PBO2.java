65fd368a7eab46c7c65bf64d32525218aa70cdb0
ramadhan <d_el12@yahoo.com>
2017-11-03 10:40:21 +0800

    mengisi attribute dari object "m

--- src/pbo2/PBO2.java
+++ src/pbo2/PBO2.java
@@ -3,6 +3,10 @@ package pbo2;
 public class PBO2 {
     public static void main(String[] args) {
         myframe m = new myframe();
+        
+        m.pesan = "halo...";
+        m.bilBulat = 5;
+        m.bilDesimal = 3.14;
     }
     
 }
